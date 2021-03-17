b54e8e0466030c043ee26a92b610d70682e8671d
freghar <freghar@dummy.tld>
2021-03-17 03:14:22 +0000

    mtc/rhs_flashlighs: remove mission dep dummy
    We'll be breaking missions anyway due to a3aa_ee_modules
    being removed.
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/misc_tiny_configs/rhs_flashlighs/dummy_mission_dep/config.cpp
+++
@@ -1,8 +0,0 @@
-/* provide old name until missions switch over */
-class CfgPatches {
-    class a3aa_mtc_rhs_map_flashlights {
-        units[] = {};
-        weapons[] = {};
-        requiredAddons[] = {};
-    };
-};
