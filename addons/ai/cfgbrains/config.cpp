d16c162b7a15e366df0535cee18e63bef7d50cab
freghar <freghar@dummy.tld>
2021-05-08 14:45:48 +0000

    ai: make suppression stick for a reasonable time
    (Even though it has no effect on aiming error.)
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/ai/cfgbrains/config.cpp
+++ addons/ai/cfgbrains/config.cpp
@@ -27,10 +27,29 @@ class CfgBrains {
                  * this is unfortunate, but necessary - otherwise any incoming
                  * fire (even point blank or overhead across building rooftops)
                  * will drastically reduce accuracy, even with ie. 0.05 here
-                 * - note that AIBrainSuppressionComponent -> SuppressionRange
-                 *   has no effect on this
+                 * - AIBrainSuppressionComponent -> SuppressionRange
+                 *   seems to have no effect whatsoever
+                 * - AIBrainSuppressionComponent -> CauseBulletCloseWeight
+                 *   is also broken (even with 0, bullets passing by still
+                 *   count towards suppression)
                  */
                 suppressionCoef = 0;
+            };
+            class AIBrainSuppressionComponent {
+                /* from 10s for courage=1 to 60s for courage=0 */
+                bestDecreaseTime = 10;
+                worstDecreaseTime = 60;
+                /* one grenade explosion = full suppression */
+                CauseExplosionWeight = 4;
+            };
+            class AIBrainCountermeasuresComponent {
+                /* min time between smokes? */
+                useSmokeGrenadeDelay = 10;
+                /* smokes thrown by non-leader group members? */
+                nonLeaderSmokeProbability = 0.5;
+                /* getSuppression > 0.9 for 8 seconds */
+                suppressionThreshold = 0.9;
+                suppressionTimerMax = 8;
             };
         };
     };
